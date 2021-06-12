7058bbbd3b888a376145d0d8922dcecf4bf51706
AbidaAli100 <abida.ali100@gmail.com>
2021-06-12 14:36:24 +0500

    finished switch case

---
+++ CLASS-1 (Conditionals)/app3.js
@@ -0,0 +1,28 @@
+/*
+  Consider the situation in which you are required to
+   check if a number is 1 or 0. If it’s 1 you need to print true.
+    Otherwise, you should print false.
+
+*/
+
+let number1 = 0
+let number2 = 1;
+let answer = (number1 + number2);
+switch (answer) {
+    case 0:
+        answer = 0;
+        console.log("condition 0 is true");
+        break;
+
+    case 1:
+        answer = 1;
+        console.log("condition 1 is true");
+        break;
+    case 3:
+        answer = 3;
+        console.log("condition 2 is true");
+        break;
+
+        default:
+        console.log("NOT SURE");
+}
