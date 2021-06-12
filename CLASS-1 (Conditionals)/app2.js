f1c43a7a72bd05bc5878f6b22a7a37def8bcaea1
AbidaAli100 <abida.ali100@gmail.com>
2021-06-12 13:03:02 +0500

    solved 2nd problem

---
+++ CLASS-1 (Conditionals)/app2.js
@@ -0,0 +1,17 @@
+/*
+You are given two numbers, num1 = 4 and num2 = 7.
+ Add both and return true if both are equal to 11.
+  Otherwise, return false, if they don’t equal 11.
+
+ */
+
+  let numb1 = 4;
+  let numb2 = 7;
+
+  
+  if(numb1 + numb2 == 11 ){
+      console.log("True")
+  }
+  else{
+      console.log("False")
+  }
