01510b56dae5f3a1a5d52d0703b3f3a21685c981
AbidaAli100 <abida.ali100@gmail.com>
2021-06-14 22:06:34 +0500

    finished problem 4

---
+++ 1- Conditionals/1- if-else/app4.js
@@ -0,0 +1,13 @@
+/*
+Suppose you are given a number and you need
+ to check whether it’s even or odd. How'd you do that?
+ */
+
+ let numb = 14;
+
+ if (numb % 2 == 0){
+     console.log("It is an even number");
+ }
+ else{
+     console.log("It is an odd number");
+ }
