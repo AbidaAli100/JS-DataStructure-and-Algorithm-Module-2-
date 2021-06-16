75d8f94b176f4096e1e44310692770799b478bfe
AbidaAli100 <abida.ali100@gmail.com>
2021-06-16 21:55:07 +0500

    finished task 5

---
+++ 2- Loops/app5.js
@@ -0,0 +1,9 @@
+/*
+Consider you are given an array [2,0,1,6,45,89].
+You need to find the smallest element in the array. How’d you do tha
+*/
+
+number = [2, 0, 1, 6, 45, 89];
+
+    console.log(Math.min(...number));
+
