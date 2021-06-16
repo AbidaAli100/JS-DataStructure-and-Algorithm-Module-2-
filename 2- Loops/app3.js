0fdf14327828207ba107596ecf73e7ae7f9d90f2
AbidaAli100 <abida.ali100@gmail.com>
2021-06-16 21:25:07 +0500

    finished problem 3

---
+++ 2- Loops/app3.js
@@ -0,0 +1,17 @@
+/*
+    Work on the problem above to dynamically print the name of 
+    each animal and prepend it with ‘this is’ conditionally.
+     Print ‘something else’ if the name doesn’t equal any of
+      the names in the list.
+*/
+petAnimals = ["cat", "rabbit", "pigeon", "parrot", "goldfish"];
+
+for (i = 0; i < petAnimals.length; i++) {
+    if (petAnimals[i] === "rabbit") {
+        console.log(petAnimals[i] + ":" + " is a rabbit");
+    }
+    else {
+        console.log(petAnimals[i] + ":" + "is not a rabbit");
+    }
+
+}
