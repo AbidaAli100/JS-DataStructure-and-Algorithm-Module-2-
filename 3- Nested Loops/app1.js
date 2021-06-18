0267eb8b896f6b33d28b1dc8a7ee2b3d38d40ea9
AbidaAli100 <abida.ali100@gmail.com>
2021-06-18 21:33:45 +0500

    finished problem 1

---
+++ 3- Nested Loops/app1.js
@@ -0,0 +1,14 @@
+/*
+   suppose you’re given an array [ [1, 2], [3, 4], [5, 6] ]. 
+    Print all the members of this two-dimensional array using 
+    for loop and while nested loops.
+ */
+
+array = [[1, 2], [3, 4], [5, 6]];
+for (let i = 0; i < array.length; i++) {
+    let j = 0;
+    while (j < 1) {
+        console.log(array[i]);
+        j++
+    }
+}
