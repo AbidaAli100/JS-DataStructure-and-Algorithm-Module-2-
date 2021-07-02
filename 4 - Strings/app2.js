fe630a8c91ffb28e5a373c9371c3bca72376bd33
AbidaAli100 <abida.ali100@gmail.com>
2021-07-02 18:52:08 +0500

    problem 2 solves

--- 4 - Strings/app2.js
+++ 4 - Strings/app2.js
@@ -2,7 +2,17 @@
     Take any two random strings and concatenate them to each other
  */
 
+
+// concating the strings while declaring variables   
 let firstName = "Abida";
 let secondName = "Ali";
 
 console.log(firstName + " " + secondName);
+
+
+// concantating by using aray and fo loop
+let strings = ["Abida", "Ali"];
+
+for (i = 0; i < strings.length; i++) {
+    console.log(strings[0] + " " + strings[1]);
+}
