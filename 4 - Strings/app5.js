b5900a76ecd7799053318959365d506359af360e
AbidaAli100 <abida.ali100@gmail.com>
2021-07-02 21:33:39 +0500

    problem 5 solved

---
+++ 4 - Strings/app5.js
@@ -0,0 +1,14 @@
+/*
+    There’s a string that’s given to you: ‘Reactj’.
+     You need to add s to its last index to make it
+      a complete name i.e. ‘Reactjs’
+*/
+
+// str = "";
+// let newStr = str.prototype("s");
+// console.log(newStr);
+
+var str1 = "Reactj";
+var str2 = "s";
+var res = str1.concat(str2);
+console.log(res);
