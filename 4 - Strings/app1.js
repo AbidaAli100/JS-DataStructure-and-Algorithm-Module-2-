718cc6847a3669083b6e36b119bbbff3f9f34d2b
AbidaAli100 <abida.ali100@gmail.com>
2021-07-02 18:17:38 +0500

    finished problem 1

---
+++ 4 - Strings/app1.js
@@ -0,0 +1,12 @@
+/*
+    Traverse your name and print its length,
+     indices, and each character.
+ */
+
+let myName = "Abida";
+console.log("length" + ":" + myName.length);
+// console.log([myName.length - 2]);
+
+for (let i = 0; i < myName.length; i++) {
+    console.log("charatcter " + ":" + myName[i])
+}
