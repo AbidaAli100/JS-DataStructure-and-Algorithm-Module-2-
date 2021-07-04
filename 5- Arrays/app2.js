02fd727ee3803ee50db6ced745f527b9005d66b1
AbidaAli100 <abida.ali100@gmail.com>
2021-07-04 22:01:32 +0500

    done with 3rd problem

--- 5- Arrays/app2.js
+++ 5- Arrays/app2.js
@@ -1,15 +1,18 @@
 /*
     Suppose you are given an array: [9, 7, 75, 32, 3234, 5, 3] .
      Find whether 3 is present in the given array.
+     If it’s there return true otherwise return false
 */
 
 let array = [9, 7, 75, 32, 3234, 5, 3];
 
 for (let i = 0; i < array.length; i++) {
     if (array[i] === 3) {
-        console.log("Yes 3 includes in the array")
+        console.log("Yes 3 includes in the array");
+        return true
     } else {
-        console.log("3 does not include ")
+        console.log("3 does not include");
+        return false;
     }
 }
 
