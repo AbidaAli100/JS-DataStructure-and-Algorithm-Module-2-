67494c452a9ddf1c24a57d6b90d1d8a011a1549a
AbidaAli100 <abida.ali100@gmail.com>
2021-07-09 00:16:02 +0500

    problem 3 solved

--- 7 - STACKS/app3.js
+++ 7 - STACKS/app3.js
@@ -10,15 +10,22 @@ class stack {
     }
     isEmpty() {
         if (this.item == 0) {
-            console.log(true);
+            console.log(true + " it is Empty");
         } else {
-            console.log(false)
+            console.log(false + " it is not Empty")
         }
-
+    }
+    isFull() {
+        if (this.item !== 0) {
+            console.log(true + "  it is Full");
+        } else {
+            console.log(false + " it is not Full")
+        }
     }
 }
 
 let myStack = new stack();
 myStack.isEmpty();
+myStack.isFull();
 
 //FALSE
