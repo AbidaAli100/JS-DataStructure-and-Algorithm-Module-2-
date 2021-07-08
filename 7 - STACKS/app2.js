f2aced6047518838cbc33c0a0270b19aeee94e4d
AbidaAli100 <abida.ali100@gmail.com>
2021-07-09 00:08:56 +0500

    problem 2 solved

--- 7 - STACKS/app2.js
+++ 7 - STACKS/app2.js
@@ -10,14 +10,24 @@
 
 class stack {
     constructor() {
-        this.item = ["Ali", "Haider"]
+        this.item = ["Ali", "Haider"];
+        this.size = 3;
     }
     push(item) {
         this.item.push(item);
         this.item.size + 1;
 
     }
+    peek() {
+        console.log(this.item[this.item.length - 1]);
+    }
+    pop() {
+        this.item.pop();
+        this.item[this.size - 1];
+    }
 }
 let myStack = new stack();
 myStack.push("Anjum");
+myStack.peek();
+myStack.pop();
 console.log(myStack);
