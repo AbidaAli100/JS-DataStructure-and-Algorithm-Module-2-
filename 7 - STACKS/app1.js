25089628a16c8347264ebb85fe1bf39d8e165e14
AbidaAli100 <abida.ali100@gmail.com>
2021-07-08 22:42:59 +0500

    problem 1 solved

---
+++ 7 - STACKS/app1.js
@@ -0,0 +1,12 @@
+/*
+    Implement Stack using an array. 
+    Initialize it with an array which will be used to store values and a
+    size property that will be used to track the size of stack.
+ */
+
+class stack {
+    constructor() {
+        this.item = [];
+    }
+}
+console.log(this.size = 0);
